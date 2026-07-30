eb07796ca53836d122c80221175a3d307117c47d
sudaisprogrammer <sudais.programmer@gmail.com>
2026-07-30 11:22:21 +0500

    Shopping cart

--- shopping-cart.cpp
+++ shopping-cart.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 using namespace std;
-
+class Product{
+    public:
+        int id;
+        char name[40];
+        float price;
+        static float totalcatvalue;
+};
 int main(){
-    
+
     return 0;
 }
